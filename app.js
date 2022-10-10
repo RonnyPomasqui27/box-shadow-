4ab7c90865370e22b1917a2f3efbacc567d77d26
RonnyPomasqui <ronny27pch@gmail.com>
2022-10-10 08:11:22 -0500

    intento para modificar los parrafos

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const element = document.querySelector(".box");
 const radius = document.querySelector("#radius");
 const Height = document.querySelector("#Height");
 const width = document.querySelector("#width");
+const paragraph = document.querySelector(".section-text-car")
 
 blur.addEventListener('click', (e)=>{
   alert('the Blur radius section causes a lot of lag be careful with that control')
@@ -23,6 +24,18 @@ rangeVertical.addEventListener('input', (f)=>{
     console.log(g)
     Spread.addEventListener('input', (h)=>{
       element.style.boxShadow = `${e.target.value}px ${f.target.value}px ${g.target.value}px ${h.target.value}px`;
+        // paragraph.addEventListener('input', (e)=>{
+        //   paragraph.innerHTML = `
+        //   <p class="txt-shadow">box-shadow: ${e.target.value}px ${f.target.value}px ${g.target.value}px ${h.target.value}px rgba(0,0,0,0.1); </p>
+        //   `
+        // });
+        // paragraph.addEventListener('input', (i)=>{
+        //   paragraph.innerHTML = `
+        //   <p class="txt-shadow">box-shadow: ${e.target.value}px ${f.target.value}px ${g.target.value}px ${h.target.value}px rgba(0,0,0,0.1);</p>
+        //   `
+        //   console.log(paragraph)
+        // });
+        
       });
     });
   });
@@ -40,3 +53,5 @@ Height.addEventListener('input', (e)=>{
 width.addEventListener('input', (e)=>{
   element.style.width = `${e.target.value}px`
 });
+
+
